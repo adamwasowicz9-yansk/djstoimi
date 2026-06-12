b3983f645b5b52af538b35420d569b2a263e5b4a
danjekb <139584970+danjekb@users.noreply.github.com>
2026-06-12 05:27:45 +0200

    Add build properties for device a52sxqxx
    Added additional build properties for device model and system information.

--- sixteen.sh
+++ sixteen.sh
@@ -70,6 +70,16 @@ BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "product" "ro.build.display.id" "[CrapUI $
 BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "system" "ro.product.model" "SM-A528B"
 BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "product" "ro.product.system.model" "SM-A528B"
 
+# === DODATKOWE PATCHES BUILD.PROP (fingerprint, opis, produkt) ===
+BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "system" "ro.product.system.name" "a52sxqxx"
+BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "product" "ro.product.system.name" "a52sxqxx"
+BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "system" "ro.product.system.device" "a52sxq"
+BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "product" "ro.product.system.device" "a52sxq"
+BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "system" "ro.system.build.fingerprint" "samsung/a52sxqxx/a52sxq:11/RP1A.200720.012/A528BXXSBGYI3:user/release-keys"
+BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "system" "ro.build.description" "a52sxqxx-user 11 RP1A.200720.012 A528BXXSBGYI3 release-keys"
+BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "system" "ro.build.product" "a52sxq"
+BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "product" "ro.build.product" "a52sxq"
+
 # ==========================================
 # CrapUI - System Tweaks
 # ==========================================
